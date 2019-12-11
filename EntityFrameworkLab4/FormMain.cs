4ef26f301de3a9dacfe5d5f3fbdde52cdf63f9a7
Jakub Kalina <jakub.kalina@codeteam.pl>
2019-12-11 01:24:03 +0100

    Basic structure

---
+++ EntityFrameworkLab4/FormMain.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkLab4
+{
+    public partial class FormMain : Form
+    {
+
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Edycja danych skoczka
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonJumperUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        /// <summary>
+        /// Usunięcie skoczka z bazy danych
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonJumperDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Wyświetlenie informacji o skoczku
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonJumperShow_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        /// <summary>
+        /// Usunięcie kraju z bazy danych
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCountryDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Edycja danych kraju
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCountryUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Wyświetlenie informacji o kraju
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCountryShow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Dodanie nowego skoczka do bazy danych
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonJumperAdd_Click(object sender, EventArgs e)
+        {
+            var jumperFirstName = textBoxJumperFirstName.Text;
+            var jumperLastName = textBoxJumperLastName.Text;
+            var jumperBirthdate = dateTimePickerJumper.Text;
+            var countryId = textBoxJumperCountryId.Text;
+
+
+        }
+
+        /// <summary>
+        /// Dodanie nowego kraju do bazy danych
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCountryAdd_Click(object sender, EventArgs e)
+        {
+            var countryName = textBoxCountryName.Text;
+            var countryRecord = textBoxCountryRecord.Text;
+
+
+        }
+
+        /// <summary>
+        /// Zapis zmian danych skoczka
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonJumperSaveChanges_Click(object sender, EventArgs e)
+        {
+            int jumperUpdatedId = Int32.Parse(textBoxUpdateJumperId.Text);
+            var jumperUpdatedFirstName = textBoxUpdateJumperFirstName.Text;
+            var jumperUpdatedLastName = textBoxUpdateJumperLastName.Text;
+            var jumperUpdatedBirthdate = dateTimePickerUpdateJumper.Text;
+            var jumperUpdatedCountryId = textBoxUpdateCountryId.Text;
+
+
+        }
+    }
+}
